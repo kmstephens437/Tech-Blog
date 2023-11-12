2b71c04dff2466b516bc52575bb9847f391cf948
Kevin-Stephens <kmstephens437@gmail.com>
2023-11-12 11:06:10 -0600

    resorting files to at least display main page

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./lib/helpers');
-const sequelize = require('./config/connection');
+//const exphbs = require('express-handlebars');
+//const routes = require('./controllers');
+//const helpers = require('./lib/helpers');
+//const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+//const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+//app.engine('handlebars', hbs.engine);
+//app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.use(routes);
 
+//app.use(routes);
+
+app.get('/',(req,res) =>
+res.sendFile(path.join(__dirname,'/public/index.html'))
+);
 
